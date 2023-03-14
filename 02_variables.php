1f4c6c0c72172d66e328d198f5d2c7b254fd1252
Exequel <exql.adzn@gmail.com>
2023-03-14 18:05:56 +0800

    add variable rules

--- 02_variables.php
+++ 02_variables.php
@@ -12,5 +12,13 @@
 - Resource - A special variable that holds a resource
 */
 
+/* --------- Variable Rules --------- */
+/*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- variables can't start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
 
 ?>
