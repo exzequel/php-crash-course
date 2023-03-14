ce274232b32bf9e4448ad79a71c2daa85622d9e2
Exequel <exql.adzn@gmail.com>
2023-03-14 18:05:34 +0800

    initialize php variables

---
+++ 02_variables.php
@@ -0,0 +1,16 @@
+<?php
+
+/* --------- PHP Data Types --------- */
+/*
+- String - A string is a series of characters surrounded by quotes
+- Integer - Whole numbers
+- Float - Decimal numbers
+- Boolean - true or false
+- Array - An array is a special variable, which can hold more than one value
+- Object - A class
+- NULL - Empty variable
+- Resource - A special variable that holds a resource
+*/
+
+
+?>
