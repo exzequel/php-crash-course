4745b6c6320e84a0ead522bfcc7bcc56ed35aae1
Exequel <exql.adzn@gmail.com>
2023-03-14 18:32:40 +0800

    add operators and if statements guide

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -1,3 +1,27 @@
 <?php
 
+/* ---- Conditionals & Operators ---- */
+
+/* ------------ Operators ----------- */
+
+/*
+  < Less than
+  > Greater than
+  <= Less than or equal to
+  >= Greater than or equal to
+  == Equal to
+  === Identical to
+  != Not equal to
+  !== Not identical to
+*/
+
+/* ---------- If Statements --------- */
+
+/*
+** If Statement Syntax
+if (condition) {
+  // code to be executed if condition is true
+}
+*/
+
 ?>
