778930c8c868d6b05ba3a01f7b39741a45e27c31
Exequel <exql.adzn@gmail.com>
2023-03-14 18:15:25 +0800

    finish variables in php

--- 02_variables.php
+++ 02_variables.php
@@ -21,4 +21,31 @@
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
 
+$name = 'Adi'; // String
+$age = 19; // int
+$has_kids = false; // bool
+$cash_on_hand = 100.50; // float
+
+// echo $has_kids; // php will not show anything if false and will output 1 if true
+// var_dump($has_kids);
+
+// echo $name . ' is ' . $age . ' years old'; // use (.) to concatenate
+// echo "$name is $age years old"; // double quotes " " will parse the variables unlike single quotes '
+// echo "${name} is ${age} years old"; // works as well recommended (same as javascript literals)
+
+$x = '5' + 5;
+// var_dump($x);
+
+/* PHP Operators */
+// echo 10 -5;
+// echo 5 * 6;
+// echo 10 / 2;
+// echo 10 % 3;
+
+// Declare a constant in PHP
+define('HOST', 'localhost');
+define('DB_NAME', 'localhost');
+
+echo HOST;
+
 ?>
