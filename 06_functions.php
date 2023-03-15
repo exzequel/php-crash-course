c00178feb9ac23a9873902e78429a2b7f3e65e8d
Exequel <exql.adzn@gmail.com>
2023-03-15 12:35:05 +0800

    add comments for guide function

--- 06_functions.php
+++ 06_functions.php
@@ -1,4 +1,13 @@
 <?php
+    /* ------------ Functions ----------- */
+
+    /*
+    ** Function Syntax
+    function functionName($arg1, $arg2, ...) {
+        // code to be executed
+    }
+    - Functions have their own local scope as opposed to global scope
+    */
 
     // $y = 12; // declared in global scope
 
@@ -12,6 +21,7 @@
     }
 
     // echo $x; // global scope (won't work)
+    // running a function
     // registerUser('Adi');
 
     function sum($n1, $n2){ // arguments (optional can set default values)
@@ -21,6 +31,7 @@
     $number = sum(5, 5); // paremeters
     // echo $number;
 
+    // assign a function to a variable
     $subtract = function($n1, $n2) {
         return $n1 - $n2;
     };
