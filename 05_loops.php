8e2ba3ed519f84dbc2cd1b92aa3543480b457425
Exequel <exql.adzn@gmail.com>
2023-03-15 08:14:51 +0800

    initialize loops php

---
+++ 05_loops.php
@@ -0,0 +1,16 @@
+<?php
+
+/* -------- Loops & Iteration ------- */
+
+/* ------------ For Loop ------------ */
+
+/*
+** For Loop Syntax
+  for (initialize; condition; increment) {
+  // code to be executed
+  }
+*/
+
+
+
+?>
